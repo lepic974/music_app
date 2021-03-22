ab84c9ab33ab924a5e61238c35c0009793c636de
Johan <johan.lepinay@gmail.com>
2021-03-22 04:53:38 +0400

    Fin de App

---
+++ lib/musique.dart
@@ -0,0 +1,13 @@
+class Musique {
+  String titre;
+  String artiste;
+  String imagePath;
+  String musicURL;
+
+  Musique(String titre, String artiste, String imagePath, String musicURL) {
+    this.titre = titre;
+    this.artiste = artiste;
+    this.imagePath = imagePath;
+    this.musicURL = musicURL;
+  }
+}
